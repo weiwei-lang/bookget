0a7c5f8ecd85b4edac6c18faf830ccfc1d5e4d39
zhudw <zhudwi@foxmail.com>
2023-02-18 12:29:15 +0800

    [修复]广州大典

--- build.sh
+++ build.sh
@@ -2,16 +2,18 @@ ver="1.0.2"
 
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o target/bookget_v${ver}_windows/bookget.exe .
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o target/bookget_v${ver}_linux/bookget .
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o target/bookget_v${ver}_macos/bookget .
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o target/bookget_v${ver}_macos/bookget_m2 .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o target/bookget_v${ver}_macOS/bookget .
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o target/bookget_v${ver}_macOs_M2/bookget .
 
 
 cp cookie.txt target/bookget_v${ver}_linux/cookie.txt
-cp cookie.txt target/bookget_v${ver}_macos/cookie.txt
+cp cookie.txt target/bookget_v${ver}_macOS/cookie.txt
+cp cookie.txt target/bookget_v${ver}_macOS_M2/cookie.txt
 cp cookie.txt target/bookget_v${ver}_windows/cookie.txt
 
 
 cd target/ 
 7za a -t7z bookget_v${ver}_windows.7z bookget_v${ver}_windows
 tar cjf bookget_v${ver}_linux.tar.bz2 bookget_v${ver}_linux
-tar cjf bookget_v${ver}_macos.tar.bz2 bookget_v${ver}_macos
+tar cjf bookget_v${ver}_macOS.tar.bz2 bookget_v${ver}_macOS
+tar cjf bookget_v${ver}_macOS_M2.tar.bz2 bookget_v${ver}_macOS_M2
